823c56ad9a345fb7317d13747a4e12c83a36cec9
lebarbe <lebarbe@ecole.ensicaen.fr>
2014-01-25 15:57:59 +0100

    Sound fixed

--- src/com/buchbuchteam/buchbuch/model/MovementQueue.java
+++ src/com/buchbuchteam/buchbuch/model/MovementQueue.java
@@ -1,27 +1,43 @@
 package com.buchbuchteam.buchbuch.model;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 
-public class MovementQueue {
+public class MovementQueue
+{
 
 	protected LinkedList<Movement> queue;
-	
-	public MovementQueue(){
+
+	public MovementQueue()
+	{
 		queue = new LinkedList<Movement>();
 	}
-	
-	public void add(Movement movement){
+
+	public void add(Movement movement)
+	{
+		if (queue.contains(movement))
+			return;
 		queue.addLast(movement);
 	}
-	
-	public Movement remove(){
+
+	public Movement remove()
+	{
 		return queue.removeFirst();
 	}
-	
-	public void Execute(){
-		//TODO Execute the queue
-		
-		queue.remove().doMove();
+
+	public void execute()
+	{
+		Iterator<Movement> i = queue.iterator();
+		while (i.hasNext())
+		{
+			Movement m = i.next();
+			m.pass();
+			if (m.getDelay() <= 0)
+			{
+				m.doMove();
+				i.remove();
+			}
+		}
 	}
-	
+
 }
--- src/com/buchbuchteam/buchbuch/model/Team.java
+++ src/com/buchbuchteam/buchbuch/model/Team.java
@@ -8,25 +8,27 @@ import com.buchbuchteam.buchbuch.model.entities.BuchBuch;
 public class Team implements Controllable
 {
 	private static Team instance;
-	
+
 	private float y;
 	protected LinkedList<BuchBuch> team;
 	protected MovementQueue movements;
 	protected int nbBuch;
+	private int runCry;
 
 	private Team()
 	{
 		this.team = new LinkedList<BuchBuch>();
 		this.movements = new MovementQueue();
 		this.y = 250;
+		this.runCry = 200;
 		reinit();
 	}
 
 	public void reinit()
 	{
 		nbBuch = 5;
-		while(team.size() < nbBuch)
-			team.add(new BuchBuch(-64, y));
+		while (team.size() < nbBuch)
+			team.add(new BuchBuch(0, y));
 	}
 
 	@Override
@@ -34,8 +36,8 @@ public class Team implements Controllable
 	{
 		for (int i = 0; i < nbBuch; i++)
 		{
-			movements.add(new Movement(Movement.MovementType.JUMP, team.get(i)));
-			movements.remove().doMove();
+			movements.add(new Movement(Movement.MovementType.JUMP, team.get(i),
+					i * 20));
 		}
 	}
 
@@ -45,11 +47,11 @@ public class Team implements Controllable
 		for (int i = 0; i < nbBuch; i++)
 		{
 			movements.add(new Movement(Movement.MovementType.CROUCH, team
-					.get(i)));
+					.get(i), i * 20));
 			movements.remove().doMove();
 		}
 	}
-	
+
 	public int getX(int num)
 	{
 		if (team.get(num) == null)
@@ -62,33 +64,44 @@ public class Team implements Controllable
 	{
 		for (int i = 0; i < team.size(); i++)
 		{
-			movements.add(new Movement(Movement.MovementType.WALK, team.get(i)));
+			movements.add(new Movement(Movement.MovementType.WALK, team.get(i),
+					i * 20));
 			movements.remove().doMove();
 		}
 	}
 
 	@Override
-	public void run() {
+	public void run()
+	{
 		for (int i = 0; i < team.size(); i++)
 		{
-			movements.add(new Movement(Movement.MovementType.RUN, team.get(i)));
-			movements.remove().doMove();
+			movements.add(new Movement(Movement.MovementType.RUN, team.get(i),
+					0));
 		}
 	}
 
 	@Override
 	public void leave()
 	{
-
 		movements.add(new Movement(Movement.MovementType.LEAVE, team
-				.removeFirst()));
+				.removeFirst(), 1));
 		nbBuch--;
 	}
 
+	
 	public void render(Batch spriteBatch, float animTime)
 	{
-		for (BuchBuch b: team)
+		movements.execute();
+		for (BuchBuch b : team)
+		{
+			runCry --;
+			if (b.isRunning() && runCry <= 0)
+			{
+				b.cry();
+				runCry = 1600;
+			}
 			spriteBatch.draw(b.getFrame(animTime), b.getX(), b.getY());
+		}
 	}
 
 	public static Team getInstance()
