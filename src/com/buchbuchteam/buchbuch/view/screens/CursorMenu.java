1c674e4f0e0d9a34104d19096f28b0f88699fd2a
steven <steven@steven-PC>
2014-01-26 04:16:39 +0100

    Supression debug menu

--- src/com/buchbuchteam/buchbuch/view/screens/CursorMenu.java
+++ src/com/buchbuchteam/buchbuch/view/screens/CursorMenu.java
@@ -16,7 +16,7 @@ public class CursorMenu {
 	private Stage stage;
 	private Sprite sprite;
 	private String file;
-	public static final int width=40, height= 40, x =250;
+	public static final int width=40, height= 40, x =310;
 	public static int Ay = 55;
 	public static final int j = 1;
 	private int y, e=1;
@@ -38,7 +38,6 @@ public class CursorMenu {
 				{
 
 					
-					System.out.println(e);
 					sprite.setPosition(x, y = y + Ay);
 					
 				
@@ -52,7 +51,6 @@ public class CursorMenu {
 				{
 
 					
-					System.out.println(e);
 					sprite.setPosition(x, y = y - Ay);
 					
 				
@@ -91,22 +89,16 @@ public class CursorMenu {
 	
 		
 		if(Gdx.input.isKeyPressed(Input.Keys.ENTER  )){
-			System.out.println("ENTER");
 			switch (e){
 			case 1: ((com.badlogic.gdx.Game) Gdx.app.getApplicationListener()).setScreen(new GameScreen());
-					System.out.println("JOUER");
 				break;
 			case 2: ((com.badlogic.gdx.Game) Gdx.app.getApplicationListener()).setScreen(new ControlScreen());
-			System.out.println("CONTROL");
 				break;
 			case 3: ((com.badlogic.gdx.Game) Gdx.app.getApplicationListener()).setScreen(new HighScoreScreen());
-			System.out.println("HIGH SCORE");
 				break;
 			case 4: ((com.badlogic.gdx.Game) Gdx.app.getApplicationListener()).setScreen(new ControlScreen());
-			System.out.println("CREDITS");
 				break;
 			case 5: Gdx.app.exit();
-			System.out.println("QUITTER");
 				break;
 				
 			default: 
