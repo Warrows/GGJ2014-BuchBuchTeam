0c3919fe2e40ab2b6dd816f20be2a2365c15f1fe
steven <steven@steven-PC>
2014-01-26 13:05:32 +0100

    Modification curseur

--- src/com/buchbuchteam/buchbuch/view/screens/CursorMenu.java
+++ src/com/buchbuchteam/buchbuch/view/screens/CursorMenu.java
@@ -2,108 +2,51 @@ package com.buchbuchteam.buchbuch.view.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
 
-public class CursorMenu
+public class CursorMenu extends Sprite
 {
 
-	private Stage stage;
-	private Sprite sprite;
-	private String file;
 	public static final int width = 40, height = 40, x = 310;
-	public static int Ay = 55;
 	public static final int j = 1;
 	private int y, e = 1;
 
 	public CursorMenu(String file)
 	{
-
-		y = MenuScreen.jouerY;
-
-		stage = new Stage();
-		sprite = new Sprite(new Texture(Gdx.files.internal(file)), width,
+		super(new Texture(Gdx.files.internal("img/menu/cursor.png")), width,
 				height);
 
-		stage.addListener(new InputListener()
-		{
-			public boolean keyDown(InputEvent event, int keyCode)
-			{
-				if (keyCode == Input.Keys.UP && e > 1)
-
-				{
-
-					sprite.setPosition(x, y = y + Ay);
-
-					e--;
-
-					return true;
-				}
-				if (keyCode == Input.Keys.DOWN && e < 5)
-				{
-
-					sprite.setPosition(x, y = y - Ay);
-
-					e++;
-
-					return true;
-				}
-				return false;
-			}
-		});
-
-		Gdx.input.setInputProcessor(stage);
-		sprite.setPosition(x, y);
+		y = MenuScreen.jouerY;	
+		setPosition(x, y);
+		
 	}
 
-	public void CursorRender()
+	
+
+	public void monter()
 	{
-
-		stage.getSpriteBatch().begin();
-		sprite.draw(stage.getSpriteBatch());
-
-		stage.getSpriteBatch().end();
+		if (e>=1)
+		y = y+47; 
+		e--;
+		setY(y);
+		
 	}
 
-	public void Choose()
+	public int getPosition()
 	{
-
-		if (Gdx.input.isKeyPressed(Input.Keys.ENTER))
-		{
-			switch (e)
-			{
-			case 1:
-				((com.badlogic.gdx.Game) Gdx.app.getApplicationListener())
-						.setScreen(GameScreen.getInstance());
-				break;
-			case 2:
-				((com.badlogic.gdx.Game) Gdx.app.getApplicationListener())
-						.setScreen(new ControlScreen());
-				break;
-			case 3:
-				((com.badlogic.gdx.Game) Gdx.app.getApplicationListener())
-						.setScreen(new HighScoreScreen());
-				break;
-			case 4:
-				((com.badlogic.gdx.Game) Gdx.app.getApplicationListener())
-						.setScreen(new CreditScreen());
-				break;
-			case 5:
-				Gdx.app.exit();
-				break;
-
-			default:
-				System.out.println("NONE");
-			}
-
-		}
-
+		return e;
 	}
+	
+	public void descendre()
+	{
+		if (e<=5)
+			y = y-47; 
+			e++;
+			setY(y);
+			}
 
 }
--- src/com/buchbuchteam/buchbuch/view/screens/MenuScreen.java
+++ src/com/buchbuchteam/buchbuch/view/screens/MenuScreen.java
@@ -1,8 +1,12 @@
 package com.buchbuchteam.buchbuch.view.screens;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.buchbuchteam.buchbuch.view.Game;
 
 public class MenuScreen extends ScreenMaster {
@@ -17,10 +21,10 @@ public class MenuScreen extends ScreenMaster {
 							controlY = jouerY - buttonHeight - espaceEntreButton,
 							hightscoreY = controlY - buttonHeight - espaceEntreButton, 
 							creditsY= hightscoreY - buttonHeight - espaceEntreButton, 
-							quitterY = creditsY - buttonHeight - espaceEntreButton ;
-	private Sprite jouerSprite, controlSprite, hightscoreSprite, creditsSprite, quitterSprite;
+							quitterY = creditsY - buttonHeight - espaceEntreButton;
+	public Stage stage;
 	
-	private BoutonMenu boutonJouer, boutonControl, boutonHightScore, boutonCredits, boutonQuitter;
+	
 	private CursorMenu curseur;
 	
 
@@ -32,10 +36,12 @@ public class MenuScreen extends ScreenMaster {
 		
 		curseur = new CursorMenu("img/menu/cursor.png");
 		
+		stage = new Stage();
 		
 		
 		
 		
+		curseur.setPosition(curseur.getX(), curseur.getY());
 		
 		
 	
@@ -45,26 +51,80 @@ public class MenuScreen extends ScreenMaster {
 	
 	private static Animation bgAnim;
 	{
-				Sprite[] bgFrames = new Sprite[2];
+		Sprite[] bgFrames = new Sprite[2];
 		bgFrames[0] = new Sprite(new Texture(
 				Gdx.files.internal("img/menu/bgmenu.png")), 0,
-				0, 128, 128);
+				0, 960, 640);
 		bgFrames[1] = new Sprite(new Texture(
 				Gdx.files.internal("img/menu/bgmenu2.png")), 0,
-				0, 128, 128);
-		bgAnim = new Animation(0.2F, bgFrames);
+				0, 960, 640);
+		
+		bgAnim = new Animation(0.6F, bgFrames);
 		bgAnim.setPlayMode(Animation.LOOP);
 	}
 	
 	
 	public void render(float delta) {
 
+		
+		
+		stage.getSpriteBatch().begin();
+			stage.getSpriteBatch().draw(bgAnim.getKeyFrame(delta), 0, 0);
+			curseur.draw(stage.getSpriteBatch());
+			
+			if (Gdx.input.isKeyPressed(Input.Keys.ENTER))
+			{
+				switch (curseur.getPosition())
+				{
+				case 1:
+					((com.badlogic.gdx.Game) Gdx.app.getApplicationListener())
+							.setScreen(GameScreen.getInstance());
+					break;
+				case 2:
+					((com.badlogic.gdx.Game) Gdx.app.getApplicationListener())
+							.setScreen(new ControlScreen());
+					break;
+				case 3:
+					((com.badlogic.gdx.Game) Gdx.app.getApplicationListener())
+							.setScreen(new HighScoreScreen());
+					break;
+				case 4:
+					((com.badlogic.gdx.Game) Gdx.app.getApplicationListener())
+							.setScreen(new CreditScreen());
+					break;
+				case 5:
+					Gdx.app.exit();
+					break;
 
-		super.bgRender();
-		
-		curseur.CursorRender();
-		curseur.Choose();
-		
+				default:
+					System.out.println("NONE");
+				}
+			}
+			stage.addListener(new InputListener()
+			{
+				public boolean keyDown(InputEvent event, int keyCode)
+				{
+					if (keyCode == Input.Keys.UP )
+
+					{
+
+						curseur.monter(); 
+						
+
+						return true;
+					}
+					if (keyCode == Input.Keys.DOWN )
+					{
+
+						curseur.descendre();
+						return true;
+					}
+					return false;
+				}
+			});
+			Gdx.input.setInputProcessor(stage);
+				
+		stage.getSpriteBatch().end();
 		
 		
 		
